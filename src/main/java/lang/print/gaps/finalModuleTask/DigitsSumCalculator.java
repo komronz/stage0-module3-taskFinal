c360de1f5115545630030a46ad67ab63972cfc8a
komronz <pandacoder0820@gmail.com>
2022-07-28 22:42:28 +0500

    module3FinalTask

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
-        int sum = 0;
+      int sum = 0;
         while(number > 0){
             sum += number % 10;
             number = number / 10;
--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,7 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        boolean a = number > 0;
+       boolean  a = (number> 0);
         System.out.println(a);
+
     }
 }
