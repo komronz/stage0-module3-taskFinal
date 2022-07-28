48c673fc5953e8e3e22a81ff68ef8ea52e9c6087
komronz <pandacoder0820@gmail.com>
2022-07-28 22:21:39 +0500

    module3FinalTask

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.util.Scanner;
+
 public class NumberReverter {
     public void revert(int number) {
+       if (number>100){
+        int first = number/100;
+        int third = number%10;
+        int second = (number/10)%10;
+           System.out.println(""+third+second+first);
+       }
+       else System.out.println(number);
     }
 }
